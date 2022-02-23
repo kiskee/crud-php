cc5f7b5bc62adb4efd15e01ea0affd4bdd4993d0
kiskee <53805976+kiskee@users.noreply.github.com>
2022-02-23 15:16:24 -0500

    Add files via upload

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+
+    session_start();
+
+
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'php_crud'
+
+);
+
+
+//if(isset($conn)){
+ //   echo 'DB is connected';
+//}
+?>
